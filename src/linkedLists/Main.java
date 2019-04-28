6bee9b44e962101f45b83b001288669bc426f13a
mpmcauliffe <mcauliffe99@gmail.com>
2019-04-27 21:46:28 -0600

    update with addBefore() method

--- src/linkedLists/Main.java
+++ src/linkedLists/Main.java
@@ -3,6 +3,9 @@ package linkedLists;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Employee joanJett = new Employee("Joan", "Jett", 673909);
+        Employee artemisEntreri = new Employee("Artemis", "Entreri", 489007);
+        Employee caitSith = new Employee("Cait", "Sith", 290276);
+        Employee gandolf = new Employee("Gandolf", "the Gray", 324501);
     }
 }
